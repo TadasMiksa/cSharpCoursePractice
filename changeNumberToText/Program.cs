0b71530ae7a5cfaab8e0931bcf0c681276a2fa64
Tadas Miksa <tadasmiksa8@gmail.com>
2019-02-05 22:34:10 +0200

    ND padaryta iki (-99;99)

--- changeNumberToText/Program.cs
+++ changeNumberToText/Program.cs
@@ -7,9 +7,9 @@ class Program
     {
         // TODO : keiskite FROM..TO skaicius pagal tai kiek spesite padaryt uzduociu. (-19...19, -99..99, ir tt.)
         // min skaicius 
-        const int FROM_NUMBER = -9;
+        const int FROM_NUMBER = -99;
         // max skaicius 
-        const int TO_NUMBER = 9;
+        const int TO_NUMBER = 99;
 
         string inputString = "";
         int inputNumber = 0;
@@ -22,6 +22,7 @@ class Program
             if (checkIfGoodNumber(inputString))
             {
                 Console.WriteLine("Skaicius teisingas!");
+
                 inputNumber = Convert.ToInt32(inputString);
                 if (checkIfNumberInRange(FROM_NUMBER, TO_NUMBER, inputNumber))
                 {
@@ -46,28 +47,360 @@ class Program
     static string changeNumberToText(int number)
     {
         // TODO : pakeiskite sita funkcija pagal savo poreiki. (tiek kiek skaiciu spesite apdorot.)
-        return changeOnesToText(number);
+        if (number >= -9 && number <= 9)
+        {
+            return changeOnesToText(number);
+        }
+        else if (number <= -10 && number >= -19 || number >= 10 && number <= 19)
+        {
+            return changeTeensToText(number);
+        }
+        else if (number <= -20 && number >= -99 || number >= 20 && number <= 99)
+        {
+            return changeTensToText(number);
+        }
+
+        return " ";
     }
 
     // funkcija gauna string skaiciu, patikrina ar skaicius teisingu formatu. Pvz: "123", "-123" grazina true. "12a3", "1-23" grazina false.
     static bool checkIfGoodNumber(string dataToCheck)
     {
-        throw new NotImplementedException("TODO: grazinkite true, jei tekstas yra teisingas skaicius.");
+        for (int i = 0; i < dataToCheck.Length; i++)
+        {
+            char simbolis = dataToCheck[i];
+            if (simbolis != '-' && simbolis != '1' && simbolis != '2' && simbolis != '3' && simbolis != '4' && simbolis != '5' && simbolis != '6' && simbolis != '7' && simbolis != '8' && simbolis != '9' && simbolis != '0')
+            {
+                return false;
+            }
+            else
+            {
+                for (i = 1; i < dataToCheck.Length; i++)
+                {
+                    char simbolis2 = dataToCheck[i];
+                    if (simbolis2 != '1' && simbolis2 != '2' && simbolis2 != '3' && simbolis2 != '4' && simbolis2 != '5' && simbolis2 != '6' && simbolis2 != '7' && simbolis2 != '8' && simbolis2 != '9' && simbolis2 != '0')
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        return true;
+                    }
+                }//geresnei apsaugai reiketu pratesti
+                return true;
+            }
+        }
+        return false;
     }
-
     // funkcija gauna true jei skaicius checkNumber yar tarp fromNumber ir toNumber (imtinai)
     private static bool checkIfNumberInRange(int fromNumber, int toNumber, int checkNumber)
     {
-        throw new NotImplementedException("TODO: Patikrinkite ar checkNumber yar tarp skaiciu fromNumber,  toNumber");
+        if (checkNumber >= fromNumber && checkNumber <= toNumber)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
-
     // funkcija gauna int skaiciu, pakeicia ji i string teksta kuri zodziais nusako skaiciu. PVZ: -1684542 turi grazint - "minus vienas milijonas sesi simtai astuoniasdesimt keturi tukstanciai penki simtai keturiasdiasimt du"
+    static string desimtys(int number)
+    {
+        string skaiciusZodziais = "";
+        switch (number)
+        {
+            case 2:
+                skaiciusZodziais = "dvidesimt";
+                break;
+            case 3:
+                skaiciusZodziais = "trisdesimt";
+                break;
+            case 4:
+                skaiciusZodziais = "keturiasdesimt";
+                break;
+            case 5:
+                skaiciusZodziais = "penkiasdesimt";
+                break;
+            case 6:
+                skaiciusZodziais = "sesiasdesimt";
+                break;
+            case 7:
+                skaiciusZodziais = "septyniasdesimt";
+                break;
+            case 8:
+                skaiciusZodziais = "astuoniasdesimt";
+                break;
+            case 9:
+                skaiciusZodziais = "devyniasdesimt";
+                break;
+        }
+        return skaiciusZodziais;
+    }
     static string changeOnesToText(int number)
     {
-        throw new NotImplementedException("TODO: grazinkite skaiciu -9...9 zodziais.");
+        string skaiciusZodziais = "";
+        char minusas = ' ';
+        string yraMinusas = "";
+        int naujasSkaicius = 0;
+        string skaicius = Convert.ToString(number);
+
+        for (int i = 0; i < skaicius.Length; i++)
+        {
+            minusas = skaicius[i];
+            if (minusas == '-')
+            {
+                break;
+            }
+        }
+        if (minusas == '-')
+        {
+            naujasSkaicius = number - number * 2;
+            yraMinusas = "minus ";
+            if (naujasSkaicius == 1)
+            {
+                skaiciusZodziais = "vienas";
+            }
+            if (naujasSkaicius == 2)
+            {
+                skaiciusZodziais = "du";
+            }
+            if (naujasSkaicius == 3)
+            {
+                skaiciusZodziais = "trys";
+            }
+            if (naujasSkaicius == 4)
+            {
+                skaiciusZodziais = "keturi";
+            }
+            if (naujasSkaicius == 5)
+            {
+                skaiciusZodziais = "penki";
+            }
+            if (naujasSkaicius == 6)
+            {
+                skaiciusZodziais = "sesi";
+            }
+            if (naujasSkaicius == 7)
+            {
+                skaiciusZodziais = "septyni";
+            }
+            if (naujasSkaicius == 8)
+            {
+                skaiciusZodziais = "astuoni";
+            }
+            if (naujasSkaicius == 9)
+            {
+                skaiciusZodziais = "devyni";
+            }
+        }
+        else
+        {
+            switch (number)
+            {
+                case 1:
+                    skaiciusZodziais = "vienas";
+                    break;
+                case 2:
+                    skaiciusZodziais = "du";
+                    break;
+                case 3:
+                    skaiciusZodziais = "trys";
+                    break;
+                case 4:
+                    skaiciusZodziais = "keturi";
+                    break;
+                case 5:
+                    skaiciusZodziais = "penki";
+                    break;
+                case 6:
+                    skaiciusZodziais = "sesi";
+                    break;
+                case 7:
+                    skaiciusZodziais = "septyni";
+                    break;
+                case 8:
+                    skaiciusZodziais = "astuoni";
+                    break;
+                case 9:
+                    skaiciusZodziais = "devyni";
+                    break;
+                case 0:
+                    skaiciusZodziais = "nulis";
+                    break;
+            }
+        }
+        return yraMinusas + skaiciusZodziais;
+    }
+    static string changeTeensToText(int number)
+    {
+        string skaiciusZodziais = "";
+        char minusas = ' ';
+        string yraMinusas = "";
+        int naujasSkaicius = 0;
+        string skaicius = Convert.ToString(number);
+        for (int i = 0; i < skaicius.Length; i++)
+        {
+            minusas = skaicius[i];
+            if (minusas == '-')
+            {
+                break;
+            }
+        }
+        if (minusas == '-')
+        {
+            naujasSkaicius = number - number * 2;
+            yraMinusas = "minus ";
+            if (naujasSkaicius == 10)
+            {
+                skaiciusZodziais = "desimt";
+            }
+            if (naujasSkaicius == 11)
+            {
+                skaiciusZodziais = "vienuolika";
+            }
+            if (naujasSkaicius == 12)
+            {
+                skaiciusZodziais = "dvylika";
+            }
+            if (naujasSkaicius == 13)
+            {
+                skaiciusZodziais = "trylika";
+            }
+            if (naujasSkaicius == 14)
+            {
+                skaiciusZodziais = "keturiolika";
+            }
+            if (naujasSkaicius == 15)
+            {
+                skaiciusZodziais = "penkiolika";
+            }
+            if (naujasSkaicius == 16)
+            {
+                skaiciusZodziais = "sesiolika";
+            }
+            if (naujasSkaicius == 17)
+            {
+                skaiciusZodziais = "septyniolika";
+            }
+            if (naujasSkaicius == 18)
+            {
+                skaiciusZodziais = "astuoniolika";
+            }
+            if (naujasSkaicius == 19)
+            {
+                skaiciusZodziais = "devyniolika";
+            }
+        }
+        else
+        {
+            switch (number)
+            {
+                case 11:
+                    skaiciusZodziais = "vienuolika";
+                    break;
+                case 12:
+                    skaiciusZodziais = "dvylika";
+                    break;
+                case 13:
+                    skaiciusZodziais = "trylika";
+                    break;
+                case 14:
+                    skaiciusZodziais = "keturiolika";
+                    break;
+                case 15:
+                    skaiciusZodziais = "penkiolika";
+                    break;
+                case 16:
+                    skaiciusZodziais = "sesiolika";
+                    break;
+                case 17:
+                    skaiciusZodziais = "septyniolika";
+                    break;
+                case 18:
+                    skaiciusZodziais = "astuoniolika";
+                    break;
+                case 19:
+                    skaiciusZodziais = "devyniolika";
+                    break;
+                case 10:
+                    skaiciusZodziais = "desimt";
+                    break;
+            }
+        }
+        return yraMinusas + skaiciusZodziais;
     }
 
-    // TODO : sukurti funkcija kuri grazina skaiciu -19...19 zodziais - changeTeensToText
+    static string changeTensToText(int number)
+    {
+        char minusas = ' ';
+        string yraMinusas = "";
+        int naujasSkaicius = 0;
+        string skaicius = Convert.ToString(number);
+
+        for (int i = 0; i < skaicius.Length; i++)
+        {
+            minusas = skaicius[i];
+            if (minusas == '-')
+            {
+                break;
+            }
+        }
+        if (minusas == '-')
+        {
+            naujasSkaicius = number - number * 2;
+            yraMinusas = "minus ";
+            string keiciamSimbolius = Convert.ToString(naujasSkaicius);
+            for (int i = 0; i < keiciamSimbolius.Length; i++)
+            {
+                char pirmasSimbolis = keiciamSimbolius[i];
+                if (pirmasSimbolis > '1')
+                {
+                    int TempNum1 = Convert.ToInt32(Char.GetNumericValue(pirmasSimbolis));
+                    for (i = 1; i < keiciamSimbolius.Length; i++)
+                    {
+                        char antrasSimbolis = keiciamSimbolius[i];
+                        if (antrasSimbolis == '0')
+                        {
+                            return yraMinusas + desimtys(TempNum1);
+                        }
+                        else if (antrasSimbolis > '0')
+                        {
+                            int TempNum2 = Convert.ToInt32(Char.GetNumericValue(antrasSimbolis));
+                            return yraMinusas + desimtys(TempNum1) + " " + changeOnesToText(TempNum2);
+                        }
+                    }
+                }
+            }
+        }
+        else
+        {
+            string keiciamSimbolius = Convert.ToString(number);
+            for (int i = 0; i < keiciamSimbolius.Length; i++)
+            {
+                char pirmasSimbolis = keiciamSimbolius[i];
+                if (pirmasSimbolis > '1')
+                {
+                    int TempNum1 = Convert.ToInt32(Char.GetNumericValue(pirmasSimbolis));
+                    for (i = 1; i < keiciamSimbolius.Length; i++)
+                    {
+                        char antrasSimbolis = keiciamSimbolius[i];
+                        if (antrasSimbolis == '0')
+                        {
+                            return desimtys(TempNum1);
+                        }
+                        else if (antrasSimbolis > '0')
+                        {
+                            int TempNum2 = Convert.ToInt32(Char.GetNumericValue(antrasSimbolis));
+                            return desimtys(TempNum1) + " " + changeOnesToText(TempNum2);
+                        }
+                    }
+                }
+            }
+        }
+        return "";
+
+    }
 
     // TODO : sukurti funkcija kuri grazina skaiciu -99...99 zodziais - changeTensToText
 
